3384ab5652110e0bc9d03f697be2df1604888b90
Sergey Fedorov <oni.strech@gmail.com>
2019-07-10 22:09:37 +0200

    Prepare package to be published

--- mix.exs
+++ mix.exs
@@ -6,6 +6,9 @@ defmodule Avrora.MixProject do
       app: :avrora,
       version: "0.1.0",
       elixir: "~> 1.6",
+      description: description(),
+      package: package(),
+      docs: docs(),
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -22,7 +25,6 @@ defmodule Avrora.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger, :inets, :ssl]
@@ -33,12 +35,37 @@ defmodule Avrora.MixProject do
   defp elixirc_paths(:test), do: ["test/support", "lib"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Run "mix help deps" to learn about dependencies.
+  defp description do
+    """
+    Avrora is an Elixir library for convenient work with AVRO messages and schemas.
+    It is highly inspired by AvroTurf Ruby gem and has just a few dependencies.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Sergey Fedorov"],
+      licenses: ["MIT"],
+      links: %{GitHub: "https://github.com/Strech/avrora"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      logo: "misc/logo.png",
+      extras: [
+        "README.md"
+      ]
+    ]
+  end
+
   defp deps do
     [
       {:jason, "~> 1.1"},
       {:avro_ex, git: "https://github.com/beam-community/avro_ex.git", sha: " 9a02fd6"},
       {:mox, "~> 0.5", only: :test},
+      {:ex_doc, "~> 0.13", only: [:docs]},
       {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false},
       {:credo, "~> 1.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.11", only: :test}
