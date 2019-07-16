0354098fd05f0f4983244bc2cdc8b56c72f17466
Sergey Fedorov <oni.strech@gmail.com>
2019-07-16 16:18:03 +0200

    Add application to the Avrora

--- lib/avrora.ex
+++ lib/avrora.ex
@@ -2,9 +2,12 @@ defmodule Avrora do
   @moduledoc File.read!("README.md")
 
   use Supervisor
+  use Application
 
   defdelegate encode(payload, opts), to: Avrora.Encoder
   defdelegate decode(payload, opts), to: Avrora.Encoder
+
+  def start(_type, _args), do: init()
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
