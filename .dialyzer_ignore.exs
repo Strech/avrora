8b853f6679bc245416b61f3861871f0a3fb63776
Sergey Fedorov <oni.strech@gmail.com>
2019-10-23 10:40:15 +0200

    Fix dialyzer issues with undef_fun

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -3,7 +3,7 @@
   #       The fix of dialyxir requires to silence too much functions and that's sad.
   #       https://github.com/klarna/erlavro/blob/3dcb4a90af88bfe297ca60781265fbba025db48d/src/avro_binary_encoder.erl#L87-L88
   {"lib/avrora/encoder.ex", :call_without_opaque},
-  {"lib/avrora/encoder.ex", :pattern_match, 118},
-  {"lib/avrora/encoder.ex", :unused_fun, 185},
-  {"lib/avrora/encoder.ex", :unused_fun, 191}
+  {"lib/avrora/encoder.ex", :pattern_match, 117},
+  {"lib/avrora/encoder.ex", :unused_fun, 184},
+  {"lib/avrora/encoder.ex", :unused_fun, 190}
 ]
