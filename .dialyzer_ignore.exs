cae733f037b0000a881b6f8f03e58690ce615907
Sergey Fedorov <oni.strech@gmail.com>
2020-01-18 01:07:20 +0100

    Update dialyxir errors ignore list

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -2,8 +2,9 @@
   # NOTE: This code doesn't pass dialyxir because of the opaque types of erlavro functions.
   #       The fix of dialyxir requires to silence too much functions and that's sad.
   #       https://github.com/klarna/erlavro/blob/3dcb4a90af88bfe297ca60781265fbba025db48d/src/avro_binary_encoder.erl#L87-L88
+  {":0:unknown_function Function ExUnit.Callbacks.on_exit/1 does not exist."},
   {"lib/avrora/encoder.ex", :call_without_opaque},
-  {"lib/avrora/encoder.ex", :pattern_match, 117},
-  {"lib/avrora/encoder.ex", :unused_fun, 184},
-  {"lib/avrora/encoder.ex", :unused_fun, 190}
+  {"lib/avrora/encoder.ex", :pattern_match, 118},
+  {"lib/avrora/encoder.ex", :unused_fun, 185},
+  {"lib/avrora/encoder.ex", :unused_fun, 191}
 ]
