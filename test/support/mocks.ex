3b5e61812c98d953f840e7df28f8b70b75ae3c2e
Sergey Fedorov <oni.strech@gmail.com>
2020-05-26 02:31:21 +0200

    Re-arrange mocks definitions

--- test/support/mocks.ex
+++ test/support/mocks.ex
@@ -1,6 +1,5 @@
+Mox.defmock(Avrora.ConfigMock, for: Avrora.Config)
 Mox.defmock(Avrora.HTTPClientMock, for: Avrora.HTTPClient)
 Mox.defmock(Avrora.Storage.FileMock, for: Avrora.Storage)
 Mox.defmock(Avrora.Storage.MemoryMock, for: [Avrora.Storage, Avrora.Storage.Transient])
 Mox.defmock(Avrora.Storage.RegistryMock, for: Avrora.Storage)
-
-Mox.defmock(Avrora.ConfigMock, for: Avrora.Config)
