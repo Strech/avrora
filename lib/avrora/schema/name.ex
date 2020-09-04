98e6ab5cbbba3b90cc72922d2f2ad543da84ed1a
Sergey Fedorov <oni.strech@gmail.com>
2020-09-04 12:33:14 +0200

    Add origin field to Avrora.Schema.Name struct

--- lib/avrora/schema/name.ex
+++ lib/avrora/schema/name.ex
@@ -3,9 +3,10 @@ defmodule Avrora.Schema.Name do
   Struct for versioned schema names like `io.confluent.Payment:42`.
   """
 
-  defstruct [:name, :version]
+  defstruct [:origin, :name, :version]
 
   @type t :: %__MODULE__{
+          origin: String.t(),
           name: String.t(),
           version: nil | integer()
         }
@@ -18,9 +19,9 @@ defmodule Avrora.Schema.Name do
   ## Examples
 
       iex> Avrora.Schema.Name.parse("Payment")
-      {:ok, %Avrora.Schema.Name{name: "Payment", version: nil}}
+      {:ok, %Avrora.Schema.Name{origin: "Payment", name: "Payment", version: nil}}
       iex> Avrora.Schema.Name.parse("io.confluent.Payment:42")
-      {:ok, %Avrora.Schema.Name{name: "io.confluent.Payment", version: 42}}
+      {:ok, %Avrora.Schema.Name{origin: "io.confluent.Payment:42", name: "io.confluent.Payment", version: 42}}
   """
   @spec parse(String.t()) :: {:ok, t()} | {:error, term()}
   def parse(payload) when is_binary(payload) do
@@ -35,7 +36,7 @@ defmodule Avrora.Schema.Name do
           end
         end
 
-      {:ok, %__MODULE__{name: Enum.at(parts, 0), version: version}}
+      {:ok, %__MODULE__{origin: payload, name: Enum.at(parts, 0), version: version}}
     end
   end
 end
