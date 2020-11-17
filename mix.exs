e0c02d965e1ce2f9eaba17597f09f3120f208c96
Sergey Fedorov <oni.strech@gmail.com>
2020-11-17 16:21:38 +0100

    Add credo to the test env

--- mix.exs
+++ mix.exs
@@ -92,7 +92,7 @@ defmodule Avrora.MixProject do
       {:mox, "~> 0.5", only: :test},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
-      {:credo, "~> 1.4", only: :dev, runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.13", only: :test}
     ]
   end
