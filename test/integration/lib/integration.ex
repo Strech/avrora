40bea80075c68c976ab941fa4c2ceb0faf4dc59d
Sergey Fedorov <oni.strech@gmail.com>
2020-12-07 23:14:10 +0100

    Add integration tests (#59)
    * Fix Avrora.Encoder typespecs
    * Change dialyzer testing command
    * Extract Formatter and Credo check as a separate check
    * Add external typespec test

---
+++ test/integration/lib/integration.ex
@@ -0,0 +1,47 @@
+defmodule Integration do
+  @moduledoc """
+  This is an End-to-End integration test. All the public interface use cases
+  will be listed here to catch the discrepancy in typespecs.
+
+  So if you run `mix dialyzer ` and receive something like this
+
+      ```
+      lib/integration.ex:41:no_return
+      Function encode_with_two_options/0 has no local return.
+      ```
+
+  it means that somewhere happen an error and the outcome of it is not specified
+  anywhere in the typespec of the test function.
+
+  In other words either the code in the library is wrong or its typespec.
+  """
+
+  defmodule Test do
+    @moduledoc false
+
+    @doc false
+    def extract_schema_with_no_options do
+      Avrora.extract_schema(<<2, 118>>)
+    end
+
+    @doc false
+    def decode_with_no_options do
+      Avrora.decode(<<2, 118>>)
+    end
+
+    @doc false
+    def decode_with_one_option do
+      Avrora.decode(<<2, 118>>, schema_name: "avrora.Record")
+    end
+
+    @doc false
+    def encode_with_one_option do
+      Avrora.encode(%{"k" => "v"}, schema_name: "avrora.Record")
+    end
+
+    @doc false
+    def encode_with_two_options do
+      Avrora.encode(%{"k" => "v"}, schema_name: "avrora.Record", format: :plain)
+    end
+  end
+end
---
+++ test/integration/mix.exs
@@ -0,0 +1,26 @@
+defmodule Integration.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :integration,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+        plt_add_apps: [:mix]
+      ],
+      deps: deps()
+    ]
+  end
+
+  def application, do: []
+
+  defp deps do
+    [
+      {:avrora, path: "../../"},
+      {:dialyxir, "~> 1.0.0", runtime: false}
+    ]
+  end
+end
