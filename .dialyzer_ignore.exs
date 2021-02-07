d297b33c9fd6841f5d30249a9ceff908e50bcb22
Sergey Fedorov <oni.strech@gmail.com>
2021-02-08 00:36:03 +0100

    Fix dialyzer ignores

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -3,6 +3,6 @@
   #       The fix of dialyxir requires to silence too much functions and that's sad.
   #       https://github.com/klarna/erlavro/blob/3dcb4a90af88bfe297ca60781265fbba025db48d/src/avro_binary_encoder.erl#L87-L88
   {":0:unknown_function Function ExUnit.Callbacks.on_exit/1 does not exist."},
-  {"lib/avrora/codec/object_container_file.ex", :opaque_match, 28},
-  {"lib/avrora/codec/object_container_file.ex", :opaque_match, 82}
+  {"lib/avrora/codec/object_container_file.ex", :opaque_match, 32},
+  {"lib/avrora/codec/object_container_file.ex", :opaque_match, 86}
 ]
