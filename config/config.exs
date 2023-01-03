5437f3e71018c02f917dbd472ffa37197cd593af
Sergey Fedorov <oni.strech@gmail.com>
2023-01-03 22:26:06 +0100

    Replace local usage of a new option

--- config/config.exs
+++ config/config.exs
@@ -4,9 +4,9 @@ config :avrora,
   schemas_path: Path.expand("./test/fixtures/schemas"),
   registry_url: nil,
   registry_auth: nil,
+  registry_user_agent: nil,
   registry_schemas_autoreg: true,
   convert_null_values: true,
-  names_cache_ttl: :infinity,
-  user_agent_header: nil
+  names_cache_ttl: :infinity
 
 config :logger, :console, format: "$time $metadata[$level] $levelpad$message\n"
