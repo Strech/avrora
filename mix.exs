9451b929d0399089dfa15fe413ff32a50cf1668f
Sergey Fedorov <oni.strech@gmail.com>
2023-07-14 14:38:59 +0200

    Bump min. version of Elixir to 1.12
    * Update CI setup and list of supported versions
      https://hexdocs.pm/elixir/compatibility-and-deprecations.html#compatibility-between-elixir-and-erlang-otp

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Avrora.MixProject do
     [
       app: :avrora,
       version: @version,
-      elixir: "~> 1.6",
+      elixir: "~> 1.12",
       description: description(),
       package: package(),
       docs: docs(),
