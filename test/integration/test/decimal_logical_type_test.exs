114000e556ce92c04d0b2d59635a91745e2f65b7
Sergey Fedorov <oni.strech@gmail.com>
2023-12-05 19:15:39 +0100

    Add integration tests for missing Decimal library
    * Update integration test dependencies

---
+++ test/integration/test/decimal_logical_type_test.exs
@@ -0,0 +1,34 @@
+defmodule Integration.DecimalLogicalTypeTest do
+  use ExUnit.Case
+
+  describe "decimal logical type" do
+    test "when decimal library is not installed" do
+      json = ~s(
+        {
+          "namespace": "io.confluent",
+          "name": "Decimal_Test",
+          "type": "record",
+          "fields": [
+            {
+              "name": "decimal",
+              "type": {
+                "type": "bytes",
+                "precision": 3,
+                "logicalType": "Decimal",
+                "scale": 2
+              }
+            }
+          ]
+        }
+      )
+
+      {:ok, _} = Avrora.start_link()
+      {:ok, schema} = Avrora.Schema.Encoder.from_json(json)
+
+      schema = %{schema | id: nil, version: nil}
+      message = <<16, 255, 255, 255, 255, 255, 254, 29, 192>>
+
+      assert {:error, :missing_decimal_module} == Avrora.Codec.Plain.decode(message, schema: schema)
+    end
+  end
+end
