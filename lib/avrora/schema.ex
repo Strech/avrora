807c6d60abbdc149d8adb8dd3e4d874e042d6ea0
Sergey Fedorov <oni.strech@gmail.com>
2024-04-24 12:46:18 +0200

    Remove obsolete Schema field

--- lib/avrora/schema.ex
+++ lib/avrora/schema.ex
@@ -3,14 +3,13 @@ defmodule Avrora.Schema do
   Convenience wrapper struct for erlavro records.
   """
 
-  defstruct [:id, :version, :full_name, :lookup_table, :json, :source]
+  defstruct [:id, :version, :full_name, :lookup_table, :json]
 
   @type t :: %__MODULE__{
           id: nil | integer(),
           version: nil | integer(),
           full_name: String.t(),
           lookup_table: reference(),
-          json: String.t(),
-          source: String.t()
+          json: String.t()
         }
 end
